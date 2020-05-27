fdb56b2ee6b063754fcc49d3eb1a224b322197af
robotad <dadamos@bcmcgroup.com>
2020-05-27 16:30:30 -0400

    Introduce docker network to link up with human review application

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 docker build --build-arg USE_DEV_CERTS=true -t hr-db .
+docker network create -d bridge ais20
 docker run \
+    --network ais20 \
     -e MYSQL_USER=hr \
     -e MYSQL_PASSWORD="hrDatabasePassword!@#$%" \
     hr-db

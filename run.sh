ae74f8a385b25b9f987cc26d4f8ef57781565eb2
robotad <dadamos@bcmcgroup.com>
2020-06-02 17:42:46 -0400

    Add error exit to run.sh #2

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --build-arg USE_DEV_CERTS=true -t humanreview-db .
+docker build --build-arg USE_DEV_CERTS=true -t humanreview-db . | tee .out
 if [ -z "$(grep 'Successfully built' .out )" ]; then echo "Error with docker build"; exit; fi
 
 docker network create -d bridge ais20

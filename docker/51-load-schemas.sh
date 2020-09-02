1f475ffa2c819c640b47a9b851eab1f278f460bf
Roger Woo <woo@ctmart-dev-08.idm.ddenet.local>
2020-09-02 20:18:24 +0000

    used all option

--- docker/51-load-schemas.sh
+++
@@ -1,7 +0,0 @@
-echo "Loading schemas ..."
-for file in /schemas/*
-do
-  echo "[ ] Processing $file"
-  mysql $mysql_flags < $file
-  echo "[*] Finished processing $file"
-done
--- oracle/202003160800_insert_sample_admin_role.sql
+++
@@ -1,6 +0,0 @@
--- Insert sample admin role_privileges and user_roles for testing --------------------------
-INSERT INTO users (username) VALUES ('admin');
-
-INSERT INTO role_privileges (role_id, privilege_id, created_by) VALUES ((SELECT id FROM roles WHERE name = 'Admin'), (SELECT id FROM privileges WHERE name = 'Admin'), (SELECT id FROM users WHERE username = 'admin'));
-
-INSERT INTO user_roles (user_id, role_id, created_by) VALUES ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM roles WHERE name = 'Admin'), (SELECT id FROM users WHERE username = 'admin'));

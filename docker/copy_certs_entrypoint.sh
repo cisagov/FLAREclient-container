f651278c2d6de8d7e0d87caac3849f1ac6f620f9
Roger Woo <woo@ctmart-dev-08.idm.ddenet.local>
2020-09-08 15:17:29 +0000

    Update config files

--- docker/copy_certs_entrypoint.sh
+++ docker/copy_certs_entrypoint.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-echo "[ ]  Copying secrets files  ... "
-if [ -d "${SECRETS_DIR}" ]
-then
-    echo "Directory ${SECRETS_DIR} exists."
-    cp ${SECRETS_DIR}* /var/opt/mysql_certs/ 2>/dev/null
-    echo "[+]  Done copying secrets files  ... "
-else
-    echo "Warning: Directory ${SECRETS_DIR} does not exists. This is ok if you are running this locally."
-fi
+#echo "[ ]  Copying secrets files  ... "
+#if [ -d "${SECRETS_DIR}" ]
+#then
+#    echo "Directory ${SECRETS_DIR} exists."
+#    cp ${SECRETS_DIR}* /var/opt/mysql_certs/ 2>/dev/null
+#    echo "[+]  Done copying secrets files  ... "
+#else
+#    echo "Warning: Directory ${SECRETS_DIR} does not exists. This is ok if you are running this locally."
+#fi
 
 
 echo "[ ]  Starting service  ... "

dd363135a85090b91e7ba532552369db8be9c3b5
tmai <trieu.mai@us-dde.net>
2020-10-19 11:32:47 -0400

    Remove authentication

--- run.sh
+++ run.sh
@@ -4,9 +4,8 @@ docker build --build-arg USE_DEV_CERTS=true -t flareclient-mongo . | tee .out
 if [ -z "$(grep 'Successfully built' .out )" ]; then echo "Error with docker build"; exit; fi
 
 docker network create -d bridge ais20
-docker rm -f flareclient-mongo
+docker rm -f flareclient-db
 docker run \
     --network ais20 \
-    -p 27017:27017 \
-    --name flareclient-mongo \
-    flareclient-mongo
+    --name flareclient-db \
+    flareclient-db

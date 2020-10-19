46141293dcb44d82e70b1059b9dab0b67afd860a
tmai <trieu.mai@us-dde.net>
2020-10-19 12:46:03 -0400

    Add port

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --build-arg USE_DEV_CERTS=true -t flareclient-mongo . | tee .out
+docker build --build-arg USE_DEV_CERTS=true -t flareclient-db . | tee .out
 if [ -z "$(grep 'Successfully built' .out )" ]; then echo "Error with docker build"; exit; fi
 
 docker network create -d bridge ais20
@@ -8,4 +8,5 @@ docker rm -f flareclient-db
 docker run \
     --network ais20 \
     --name flareclient-db \
+    -p 27017:27017 \
     flareclient-db

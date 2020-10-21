f352ab248cc40d409fcc021c6dfcce0e28e5ecf4
tmai <trieu.mai@us-dde.net>
2020-10-21 12:49:37 -0400

    Split the build and docker run

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --build-arg USE_DEV_CERTS=true -t flareclient-db . | tee .out
-if [ -z "$(grep 'Successfully built' .out )" ]; then echo "Error with docker build"; exit; fi
-
 docker network create -d bridge ais20
 docker rm -f flareclient-db
 docker run \

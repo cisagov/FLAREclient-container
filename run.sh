8496ba6f0b50c4452f26adbeb009ad6cae8030bb
tmai <trieu.mai@us-dde.net>
2020-10-23 14:32:53 -0400

    Update mongo admin password

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ docker network create -d bridge ais20
 docker rm -f flareclient-db
 docker run \
     --network ais20 \
+    -e MONGODB_ADMIN_USERNAME="admin" \
+    -e MONGODB_ADMIN_PASSWORD="QWASZX23wesdxc" \
     --name flareclient-db \
     -p 27017:27017 \
     flareclient-db

7fd6115d8c5d82aa7e781df36ab14a3c017c839f
Brendan Sheets <bsheets@ctm-dev-23.dev.dso.ncps.us-cert.gov>
2021-12-16 23:50:49 +0000

    Add readme

--- build-docker-image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build --build-arg USE_DEV_CERTS=true -t flareclient-db . | tee .out
-if [ -z "$(grep 'Successfully built' .out )" ]; then echo "Error with docker build"; exit; fi
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker network create -d bridge ais20
-docker rm -f flareclient-db
-docker run -d \
-    --network ais20 \
-    --name flareclient-db \
-    -p 27017:27017 \
-    flareclient-db

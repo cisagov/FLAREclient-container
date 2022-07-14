eeb6c53296618fe79afc360c5c1742bd063699b0
Christopher Woodall <woodall.christopher@gmail.com>
2022-07-14 15:35:47 -0400

    Move the docker setup script

---
+++ docker/docker-import.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mkdir certs && cd "$_"
+
+keytool -genkey  -alias flareclient -storetype PKCS12 -keystore keystore.p12 -dname cn=flareclient -validity 3650 -storepass QWASZX23wesdxc -keypass QWASZX23wesdxc
+
+# TODO: Consider using `jq` instead.
+curl -s https://api.github.com/repos/cisagov/FLAREclient-container/releases/latest 2>&1 \
+  | grep browser_download_url \
+  | cut -d '"' -f 4 \
+  | wget -qO - -i - \
+  | docker image load --input -
+  # | docker import - ais-client:latest
+
+
+# docker-compose up -d
+
+# docker-compose down
